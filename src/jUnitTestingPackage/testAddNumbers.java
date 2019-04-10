12f995f2f76124341d19a9171d3d98ad9e492957
is4832 <is4832@CSELAB01-27.bennett.edu.in>
2019-04-10 11:44:23 +0530

    jUnittesting

---
+++ src/jUnitTestingPackage/testAddNumbers.java
@@ -0,0 +1,17 @@
+package jUnitTestingPackage;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class testAddNumbers {
+
+	@Test
+	public void test() {
+		jUnitFunction jtest =new jUnitFunction();
+		int result= jtest.addnumbers(80, 70);
+		assertEquals(150,result);
+
+	}
+
+}
---
+++ src/jUnitTestingPackage/testAddStrings.java
@@ -0,0 +1,17 @@
+package jUnitTestingPackage;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class testAddStrings {
+
+	@Test
+	public void test() {
+		jUnitFunction teststring=new jUnitFunction();
+		String result=teststring.addStrings("Software", "Engineering");
+		assertEquals("SoftwareEngineering",result);
+		}
+
+	}
+
